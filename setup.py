84831273c9a9ee008ea08fb0c7eb018aa12be861
Pēteris Caune <cuu508@gmail.com>
2023-12-06 15:54:51 +0200

    Fix MANIFEST.in to include the py.typed marker in source package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="oncalendar",
-    version="1.0a1",
+    version="1.0a2",
     url="https://github.com/cuu508/oncalendar",
     license="BSD",
     author="Pēteris Caune",
